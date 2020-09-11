f15e64abf246c1bf9cc5a6f760ec329d26a4c2c5
Michael NDJINGA <ndjinga@localhost.localdomain>
2020-09-11 19:37:10 +0200

    Updated to new vtk default extension _0.vtu

--- CDMATH/postprocessing/VTK_routines.py
+++ CDMATH/postprocessing/VTK_routines.py
@@ -37,11 +37,12 @@ def Extract_VTK_data_over_line_to_txt_file(inputFileName, outputFileName, point1
 def Extract_field_data_over_line_to_numpyArray(field, point1, point2, resolution):
 
     inputFileName = field.getName()#os.getcwd()+field.get_name()
-    field.writeVTK(inputFileName)
+    inputFileName_removeSpaces=inputFileName.replace(" ", "")
+    field.writeVTK(inputFileName_removeSpaces)
 
-    numpy_array = Extract_VTK_data_over_line_to_numpyArray(inputFileName+"_1.vtu", point1, point2, resolution)
+    numpy_array = Extract_VTK_data_over_line_to_numpyArray(inputFileName_removeSpaces+"_0.vtu", point1, point2, resolution)
 
-    os.remove(inputFileName+"_1.vtu")
+    os.remove(inputFileName_removeSpaces+"_0.vtu")
     return numpy_array
 
 def Extract_field_data_over_line_to_txt_file(field, point1, point2, resolution, outputFileName):
@@ -89,9 +90,9 @@ def Slice_field_data_to_numpyArray(field,
     inputFileName = field.getName()
     field.writeVTK(inputFileName)
  
-    numpy_array = Slice_VTK_data_to_numpyArray(inputFileName+"_1.vtu", point, normal, resolution)
+    numpy_array = Slice_VTK_data_to_numpyArray(inputFileName+"_0.vtu", point, normal, resolution)
 
-    os.remove(inputFileName+"_1.vtu")
+    os.remove(inputFileName+"_0.vtu")
     return numpy_array
 
 def Slice_field_data_to_txt_file(field, outputFileName,
