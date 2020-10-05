94b71631ee6444908875000e7e05f146f0546b1c
michael <michael@localhost.localdomain>
2020-10-05 19:05:17 +0200

    Corrected error in test 43 ExamplePoisson_2DEF_SQUARE (indentation and mesh location)

--- CDMATH/tests/examples/Poisson2DEF_DISK/FiniteElements2DPoisson_DISK.py
+++ CDMATH/tests/examples/Poisson2DEF_DISK/FiniteElements2DPoisson_DISK.py
@@ -110,7 +110,7 @@ for i in range(nbCells):
 			#Ajout de la contribution de la cellule triangulaire i au second membre du noeud j 
 			RHS[j_int]=Ci.getMeasure()/3*my_RHSfield[j]+RHS[j_int] # intégrale dans le triangle du produit f x fonction de base
 			#Contribution de la cellule triangulaire i à la ligne j_int du système linéaire
- 			for k in [nodeId0,nodeId1,nodeId2] : 
+			for k in [nodeId0,nodeId1,nodeId2] : 
 				if boundaryNodes.count(k)==0 : #seuls les noeuds intérieurs contribuent à la matrice du système linéaire
 					k_int=interiorNodes.index(k)#indice du noeud k en tant que noeud intérieur
 					Rigidite.addValue(j_int,k_int,GradShapeFuncs[j]*GradShapeFuncs[k]/Ci.getMeasure())
