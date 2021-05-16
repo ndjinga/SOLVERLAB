e4682337b6d89a523a763082277d2dbb7e00e57e
michael <michael@localhost.localdomain>
2021-05-16 22:07:17 +0200

    Plot all te fields of the riemann problem

--- CoreFlows/examples/Python/SinglePhase/SinglePhase_1DRiemannProblem_Implicit_LineSearch.py
+++ CoreFlows/examples/Python/SinglePhase/SinglePhase_1DRiemannProblem_Implicit_LineSearch.py
@@ -8,27 +8,6 @@ from numpy.linalg import norm
 import VTK_routines
 import exact_rs_stiffenedgas
 
-def exact_sol_Riemann_problem(xmin,xmax, t, gamma, p0, WL, WR, offset, numsamples = 100):#offset= position of the initial discontinuity
-	print("")
-	print("Determination of the exact solution of the Riemann problem for the Euler equations, gamma=", gamma, ", p0= ", p0)
-
-	RS = exact_rs_stiffenedgas.exact_rs_stiffenedgas(gamma, gamma, p0, p0);
-	RS.solve_RP(WL,WR);
-
-	delx = (xmax - xmin)/numsamples;
-	
-	density  = [0.]*numsamples
-	velocity  = [0.]*numsamples
-	pressure = [0.]*numsamples
-
-	for i in range(numsamples):
-		S = i*delx/t;
-		soln = RS.sample_solution(WL, WR, S - offset/t);
-		density[i] = soln[0]
-		pressure[i]= soln[2]
-
-	return density, velocity, pressure
-	
 def SinglePhase_1DRiemannProblem_Implicit_LineSearch():
 
 	spaceDim = 1;
@@ -104,39 +83,95 @@ def SinglePhase_1DRiemannProblem_Implicit_LineSearch():
     # evolution
 	myProblem.initialize();
 
-    #Postprocessing
-	plt.xlabel('x')
-	plt.ylabel('Pressure')
-	plt.xlim(xinf,xsup)
-	plt.ylim( initialPressure_Right - 0.1*(initialPressure_Left-initialPressure_Right), initialPressure_Left +  0.5*(initialPressure_Left-initialPressure_Right) )
-	plt.title('Solving Riemann problem for Euler equations\n with Finite volume schemes method')
-
+    ### Postprocessing initial data
 	dx=(xsup-xinf)/nx
 	x=[ i*dx for i in range(nx+1)]   # array of cell center (1D mesh)
+	fig, ([axDensity, axPressure], [axVelocity, axTemperature]) = plt.subplots(2, 2,sharex=True, figsize=(10,10))
+	plt.gcf().subplots_adjust(wspace = 0.5)
+
+	myEOS = myProblem.getFluidEOS()## Needed to retrieve gamma, pinfnity, convert (p,T) to density and (p, rho) to temperature
+
+	axPressure.set(xlabel='x (m)', ylabel='Pressure (bar)')
+	axPressure.set_xlim(xinf,xsup)
+	axPressure.set_ylim( initialPressure_Right - 0.1*(initialPressure_Left-initialPressure_Right), initialPressure_Left +  0.5*(initialPressure_Left-initialPressure_Right) )
 
 	myPressureField = myProblem.getPressureField()
 	myPressureField.writeVTK("PressureField")
 	pressureArray=VTK_routines.Extract_VTK_data_over_line_to_numpyArray("PressureField"+"_0.vtu", [xinf,0,0], [xsup,0,0],nx)
-	plt.plot(x, pressureArray,  label='Pressure time step 0')
+	axPressure.plot(x, pressureArray,  label='Pressure time step 0')
 
+	initialDensity_Left = myEOS.getDensity( initialPressure_Left, initialTemperature_Left)
+	initialDensity_Right = myEOS.getDensity( initialPressure_Right, initialTemperature_Right)
+
+	axDensity.set(xlabel='x (m)', ylabel='Density (Kg/m3)')
+	axDensity.set_xlim(xinf,xsup)
+	axDensity.set_ylim( initialDensity_Right - 0.1*(initialDensity_Left-initialDensity_Right), initialDensity_Left +  0.5*(initialDensity_Left-initialDensity_Right) )
+
+	myDensityField = myProblem.getDensityField()
+	myDensityField.writeVTK("DensityField")
+	densityArray=VTK_routines.Extract_VTK_data_over_line_to_numpyArray("DensityField"+"_0.vtu", [xinf,0,0], [xsup,0,0],nx)
+	axDensity.plot(x, densityArray,  label='Density time step 0')
+
+	axVelocity.set(xlabel='x (m)', ylabel='Velocity (m/s)')
+	axVelocity.set_xlim(xinf,xsup)
+	axVelocity.set_ylim( 0.9*initialVelocity_Right - 0.1*(initialVelocity_Left-initialVelocity_Right), 22*initialVelocity_Left +  0.5*(initialVelocity_Left-initialVelocity_Right) )
+
+	myVelocityField = myProblem.getVelocityXField()
+	myVelocityField.writeVTK("VelocityField")
+	velocityArray=VTK_routines.Extract_VTK_data_over_line_to_numpyArray("VelocityField"+"_0.vtu", [xinf,0,0], [xsup,0,0],nx)
+	axVelocity.plot(x, velocityArray,  label='Velocity time step 0')
+	
+	axTemperature.set(xlabel='x (m)', ylabel='Temperature (K)')
+	axTemperature.set_xlim(xinf,xsup)
+	axTemperature.set_ylim( 0.999*initialTemperature_Right - 0.1*(initialTemperature_Left-initialTemperature_Right), 1.001*initialTemperature_Left +  0.5*(initialTemperature_Left-initialTemperature_Right) )
+
+	myTemperatureField = myProblem.getTemperatureField()
+	myTemperatureField.writeVTK("TemperatureField")
+	temperatureArray=VTK_routines.Extract_VTK_data_over_line_to_numpyArray("TemperatureField"+"_0.vtu", [xinf,0,0], [xsup,0,0],nx)
+	axTemperature.plot(x, temperatureArray,  label='Temperature time step 0')
+	
+	### run simulation
 	ok = myProblem.run();
 
+	#Determine exact solution
+	exactDensity, exactVelocity, exactPressure = exact_rs_stiffenedgas.exact_sol_Riemann_problem(xinf, xsup, myProblem.presentTime(), myEOS.constante("gamma"), myEOS.constante("p0"), [ initialDensity_Left, initialVelocity_Left, initialPressure_Left ], [ initialDensity_Right, initialVelocity_Right, initialPressure_Right ], (xinf+xsup)/2, nx+1)
+
+	### Plot curves
+	axPressure.plot(x, exactPressure,  label='Exact Pressure ')
 	myPressureField = myProblem.getPressureField()
 	myPressureField.writeVTK("PressureField")
 	pressureArray=VTK_routines. Extract_VTK_data_over_line_to_numpyArray("PressureField_"+str(myProblem.getNbTimeStep())+".vtu", [xinf,0,0], [xsup,0,0],nx)
-	plt.plot(x, pressureArray,  label='Pressure time step '+str(myProblem.getNbTimeStep()))
+	axPressure.plot(x, pressureArray,  label='Pressure time step '+str(myProblem.getNbTimeStep()))
+	axPressure.legend()
 
-	#Determine exact solution
-	myEOS = myProblem.getFluidEOS()
-	initialDensity_Left = myEOS.getDensity( initialPressure_Left, initialTemperature_Left)
-	initialDensity_Right = myEOS.getDensity( initialPressure_Right, initialTemperature_Right)
-	exactDensity, velocity, exactPressure = exact_sol_Riemann_problem(xinf, xsup, myProblem.presentTime(), myEOS.constante("gamma"), myEOS.constante("p0"), [ initialDensity_Left, initialVelocity_Left, initialPressure_Left ], [ initialDensity_Right, initialVelocity_Right, initialPressure_Right ], (xinf+xsup)/2, nx+1)
+	axDensity.plot(x, exactDensity,  label='Exact Density ')
+	myDensityField = myProblem.getDensityField()
+	myDensityField.writeVTK("DensityField")
+	densityArray=VTK_routines. Extract_VTK_data_over_line_to_numpyArray("DensityField_"+str(myProblem.getNbTimeStep())+".vtu", [xinf,0,0], [xsup,0,0],nx)
+	axDensity.plot(x, densityArray,  label='Density time step '+str(myProblem.getNbTimeStep()))
+	axDensity.legend()
 
-	#Plot curves
-	plt.plot(x, exactPressure,  label='Exact Pressure ')
-	plt.legend()
+	axVelocity.plot(x, exactVelocity,  label='Exact Velocity ')
+	myVelocityField = myProblem.getVelocityXField()
+	myVelocityField.writeVTK("VelocityField")
+	velocityArray=VTK_routines. Extract_VTK_data_over_line_to_numpyArray("VelocityField_"+str(myProblem.getNbTimeStep())+".vtu", [xinf,0,0], [xsup,0,0],nx)
+	axVelocity.plot(x, velocityArray,  label='Velocity time step '+str(myProblem.getNbTimeStep()))
+	axVelocity.legend()
+
+	exactTemperature = [0.]*(nx+1)
+	for i in range(nx+1):
+		exactTemperature[i] = myEOS.getTemperatureFromPressure(exactPressure[i], exactDensity[i])
+
+	axTemperature.plot(x, exactTemperature,  label='Exact Temperature ')
+	myTemperatureField = myProblem.getTemperatureField()
+	myTemperatureField.writeVTK("TemperatureField")
+	temperatureArray=VTK_routines. Extract_VTK_data_over_line_to_numpyArray("TemperatureField_"+str(myProblem.getNbTimeStep())+".vtu", [xinf,0,0], [xsup,0,0],nx)
+	axTemperature.plot(x, temperatureArray,  label='Temperature time step '+str(myProblem.getNbTimeStep()))
+	axTemperature.legend()
+
+	#plt.title('Solving Riemann problem for Euler equations\n with Finite volume schemes method')
 	plt.savefig(fileName+".png")
-
+	
 	#Compute numerical error
 	error_pressure = norm( exactPressure - pressureArray )/norm( exactPressure )
 	print('relative error on pressure = ', error_pressure )
