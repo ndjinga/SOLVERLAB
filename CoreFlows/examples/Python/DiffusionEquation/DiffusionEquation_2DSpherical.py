85ee2f731bb983a04d23e980c04219771bf8f788
michael <michael@localhost.localdomain>
2021-10-18 02:45:06 +0200

    Added 2D tests for diffusion equation. To be debugged

---
+++ CoreFlows/examples/Python/DiffusionEquation/DiffusionEquation_2DSpherical.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import CoreFlows
+import cdmath
+import sys
+
+def DiffusionEquation_2DSpherical(FECalculation):
+
+    # Prepare for the mesh
+	inputfile="../resources/BoxWithMeshWithTriangularCells";
+	fieldName="Temperature";
+	spaceDim=2
+	
+    # set the limit field for each boundary
+	Temperature=623;
+	cp_ur=300
+	rho_ur=10000
+	lambda_ur=5
+
+	myProblem = CoreFlows.DiffusionEquation(spaceDim,FECalculation,rho_ur,cp_ur,lambda_ur);
+
+     #Set heat exchanges
+	fluidTemp=573.;#fluid mean temperature
+	heatTransfertCoeff=1000.;#fluid/solid exchange coefficient
+	phi=1e5;#heat power ddensity
+	myProblem.setFluidTemperature(fluidTemp);
+	myProblem.setHeatTransfertCoeff(heatTransfertCoeff);
+	myProblem.setHeatSource(phi);
+
+    #Initial field load
+	print("Loading unstructured mesh and initial data" )
+	if( FECalculation):
+		myProblem.setInitialField(inputfile,fieldName,0, cdmath.NODES)
+	else:
+		myProblem.setInitialField(inputfile,fieldName,0, cdmath.CELLS)
+
+    # the boundary conditions :
+	if( FECalculation):
+		boundaryNodeGroupNames=myProblem.getMesh().getNameOfNodeGroups()
+		print(len(boundaryNodeGroupNames), " Boundary Node Group detected : ", boundaryNodeGroupNames)
+	else:
+		boundaryFaceGroupNames=myProblem.getMesh().getNameOfFaceGroups()
+		print(len(boundaryFaceGroupNames), " Boundary Face Group detected : ", boundaryFaceGroupNames)
+
+	myProblem.setNeumannBoundaryCondition("GAUCHE");
+	myProblem.setNeumannBoundaryCondition("DROITE");
+	myProblem.setNeumannBoundaryCondition("HAUT");
+	myProblem.setNeumannBoundaryCondition("BAS");
+
+    # set the numerical method
+	myProblem.setTimeScheme( CoreFlows.Explicit);
+	# myProblem.setLinearSolver(GMRES,ILU,True);
+
+    # name of result file
+	if( FECalculation):
+		fileName = "2DSpherical_FE";
+	else:
+		fileName = "2DSpherical_FV";
+
+    # computation parameters
+	MaxNbOfTimeStep = 3 ;
+	freqSave = 1;
+	cfl = 0.95;
+	maxTime = 100000000;
+	precision = 1e-6;
+
+	myProblem.setCFL(cfl);
+	myProblem.setPrecision(precision);
+	myProblem.setMaxNbOfTimeStep(MaxNbOfTimeStep);
+	myProblem.setTimeMax(maxTime);
+	myProblem.setFreqSave(freqSave);
+	myProblem.setFileName(fileName);
+
+    # evolution
+	myProblem.initialize();
+	print("Running python "+ fileName );
+
+	ok = myProblem.run();
+	if (ok):
+		print( "Simulation python " + fileName + " is successful !" );
+		pass
+	else:
+		print( "Simulation python " + fileName + "  failed ! " );
+		pass
+
+	print( "------------ End of calculation !!! -----------" );
+
+	myProblem.terminate();
+	return ok
+
+if __name__ == """__main__""":
+    if len(sys.argv) >1 :
+        FECalculation = bool(int(sys.argv[1]))
+        DiffusionEquation_2DSpherical(FECalculation)
+    else :
+        raise ValueError("DiffusionEquation_2DSpherical : missing one argument")
