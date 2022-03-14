b735914308a87caf3bd38b84057b10da94406087
NDJINGA Michael <ndjinga@is224593.intra.cea.fr>
2022-03-14 13:30:26 +0100

    Added a new example to plot the spectrum of the transport operatror

---
+++ CDMATH/tests/examples/TransportEquation/TransportEquation/SpectrumTransportEquationUpwind.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*
+
+#===============================================================================================================================
+# Name        : Calcul VF du spectre de l'opérateur de transport 2D \ddt u + \vec a\cdot\nabla u = 0 
+# Author      : Michaël Ndjinga
+# Copyright   : CEA Saclay 2022
+# Description : Utilisation de la méthode des volumes finis avec champs discrétisés aux cellules d'un maillage quelconque
+#		        Création et sauvegarde des champs résultant en utilisant la librairie CDMATH
+#================================================================================================================================
+
+from math import sin, cos, pi, sqrt
+import time, json
+import cdmath
+import PV_routines
+import VTK_routines
+
+precision=1e-5
+
+def upwinding_coeff(normal, coeff, velocity):
+    dim=normal.size()
+    
+    if(velocity*normal<0.):
+        return velocity*normal*coeff
+    else:
+        return 0.
+    
+    
+def computeDivergenceMatrix(my_mesh,nbVoisinsMax,test_bc,velocity):
+    nbCells = my_mesh.getNumberOfCells()
+    dim=my_mesh.getMeshDimension()
+    nbComp=1
+    normal=cdmath.Vector(dim)
+
+    implMat=cdmath.SparseMatrixPetsc(nbCells*nbComp,nbCells*nbComp,(nbVoisinsMax+1)*nbComp)
+
+    for j in range(nbCells):#On parcourt les cellules
+        Cj = my_mesh.getCell(j)
+        nbFaces = Cj.getNumberOfFaces();
+
+        for k in range(nbFaces) :
+            indexFace = Cj.getFacesId()[k];
+            Fk = my_mesh.getFace(indexFace);
+            for i in range(dim) :
+                normal[i] = Cj.getNormalVector(k, i);#normale sortante
+
+            Am=upwinding_coeff( normal,Fk.getMeasure()/Cj.getMeasure(),velocity);
+
+            cellAutre =-1
+            if ( not Fk.isBorder()) :
+                # hypothese: La cellule d'index indexC1 est la cellule courante index j */
+                if (Fk.getCellsId()[0] == j) :
+                    # hypothese verifiée 
+                    cellAutre = Fk.getCellsId()[1];
+                elif(Fk.getCellsId()[1] == j) :
+                    # hypothese non verifiée 
+                    cellAutre = Fk.getCellsId()[0];
+                else :
+                    raise ValueError("computeFluxes: problem with mesh, unknown cell number")
+                    
+                implMat.addValue(j*nbComp,cellAutre*nbComp,Am)
+                implMat.addValue(j*nbComp,        j*nbComp,Am*(-1.))
+            else  :
+                if( test_bc=="Periodic" and Fk.getGroupName() != "Neumann"):#Periodic boundary condition unless Wall/Neumann specified explicitly
+                    indexFP = my_mesh.getIndexFacePeriodic(indexFace, my_mesh.getName()== "squareWithBrickWall", my_mesh.getName()== "squareWithHexagons")
+                    Fp = my_mesh.getFace(indexFP)
+                    cellAutre = Fp.getCellsId()[0]
+                    
+                    implMat.addValue(j*nbComp,cellAutre*nbComp,Am)
+                    implMat.addValue(j*nbComp,        j*nbComp,Am*(-1.))
+                elif(test_bc!="Neumann" and Fk.getGroupName() != "Neumann"):#Nothing to do for Neumann boundary condition
+                    print( Fk.getGroupName() )
+                    raise ValueError("computeFluxes: Unknown boundary condition name");
+                
+    return implMat
+
+
+def solveSpectrum(my_mesh, meshName, resolution, meshType, cfl, test_bc):
+    print( "Spectrum of the Transport Equation in dimension ", my_mesh.getMeshDimension() )
+    print( "Numerical method : ", "Upwind" )
+    print( "Mesh name : ",meshName , ", ", my_mesh.getNumberOfCells(), " cells" )
+    
+    
+    dim=my_mesh.getMeshDimension()
+    velocity=cdmath.Vector(dim)
+    for i in range(dim) :
+        velocity[i] = 1
+
+    nbCells = my_mesh.getNumberOfCells()
+    
+    nbVoisinsMax=my_mesh.getMaxNbNeighbours(cdmath.CELLS)
+    
+    dx_min=my_mesh.minRatioVolSurf()
+
+    dt = cfl * dx_min / velocity.norm()
+
+    divMat=computeDivergenceMatrix(my_mesh,nbVoisinsMax,test_bc,velocity)
+    #Adding the identity matrix on the diagonal
+    divMat.diagonalShift(1/dt)#only after  filling all coefficients
+    
+    divMat.viewMatrix(True, 0, "FiniteVolumesMatrixOn"+meshName+"_TransportEquation")
+    divMat.plotEigenvalues("FiniteVolumesEigenvaluesOn"+meshName+"_TransportEquation")
+
+
+def solve_file( filename,meshName, resolution,meshType, cfl, test_bc):
+    my_mesh = cdmath.Mesh(filename+".med")
+
+    return solveSpectrum(my_mesh, meshName+str(my_mesh.getNumberOfCells()),resolution, meshType, cfl, test_bc)
+    
+
+if __name__ == """__main__""":
+    M1=cdmath.Mesh(0.,1.,15,0.,1.,15)
+    cfl=100000
+    solveSpectrum(M1,"SquareRegularTriangles",100,"Regular triangles",cfl,"Periodic")
