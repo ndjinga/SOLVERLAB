85b6d6a3d772b5920709f849318dec542fca999f
Michael Ndjinga <michael.ndjinga@cea.fr>
2022-09-12 17:58:10 +0200

    SImplified test (smaller jump)

--- CoreFlows/examples/C/IsothermalSinglePhase_1DRiemannProblem_Implicit.cxx
+++ CoreFlows/examples/C/IsothermalSinglePhase_1DRiemannProblem_Implicit.cxx
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	cout << "Building Cartesian mesh " << endl;
 	double xinf=0.0;
 	double xsup=1.0;
-	int nx=2;
+	int nx=10;
 	Mesh M(xinf,xsup,nx);
 	double eps=1.E-8;
 	M.setGroupAtPlan(xsup,0,eps,"LeftBoundary");
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 	double initialVelocity_Left=1;
 	double initialPressure_Left=155e5;
 	double initialVelocity_Right=1;
-	double initialPressure_Right=157e5;
+	double initialPressure_Right=155.0001e5;
 
 	IsothermalSinglePhase  myProblem(Liquid,around155bars600K,spaceDim);
 	// Prepare for the initial condition
@@ -48,14 +48,14 @@ int main(int argc, char** argv)
 	myProblem.setNumericalScheme(upwind, Implicit);
 
 	// name file save
-	string fileName = "1DRiemannProblem";
+	string fileName = "1DRiemannProblem_implicit";
 
 	// parameters calculation
 	unsigned MaxNbOfTimeStep = 3;
 	int freqSave = 1;
 	double cfl = 0.95;
 	double maxTime = 5;
-	double precision = 1e-8; 
+	double precision = 1e-6; 
 	myProblem.setCFL(cfl);
 	myProblem.setPrecision(precision);
 	myProblem.setMaxNbOfTimeStep(MaxNbOfTimeStep);
