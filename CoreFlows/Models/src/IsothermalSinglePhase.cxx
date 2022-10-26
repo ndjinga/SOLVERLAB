3286cc1225de1872f2f47e335488b447d0cc998f
Michael Ndjinga <michael.ndjinga@cea.fr>
2022-10-26 12:13:44 +0200

    Corrected name of result files for model IsothermalSinglePhase

--- CoreFlows/Models/src/IsothermalSinglePhase.cxx
+++ CoreFlows/Models/src/IsothermalSinglePhase.cxx
@@ -1570,8 +1570,8 @@ void IsothermalSinglePhase::save(){
     PetscPrintf(PETSC_COMM_WORLD,"Saving numerical results at time step number %d \n\n", _nbTimeStep);
     *_runLogFile<< "Saving numerical results at time step number "<< _nbTimeStep << endl<<endl;
 
-	string prim(_path+"/SinglePhasePrim_");///Results
-	string cons(_path+"/SinglePhaseCons_");
+	string prim(_path+"/IsothermalSinglePhasePrim_");///Results
+	string cons(_path+"/IsothermalSinglePhaseCons_");
 	string allFields(_path+"/");
 	prim+=_fileName;
 	cons+=_fileName;
