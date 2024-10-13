a219510145b3c78fcedfbb9ebe6cdb081f1bd48c
COIFFIER Esteban 270863 <ec270863@is244421.intra.cea.fr>
2024-10-13 11:50:42 +0200

    ajout cas test Riemann 1d Euler baro

---
+++ CoreFlows/examples/C/EulerBarotropicStaggered/WaveStaggered_2DCylinderDeflection.cxx
@@ -0,0 +1,185 @@
+#include "WaveStaggered.hxx"
+#include "math.h"
+#include <cassert>
+
+using namespace std;
+
+std::vector<double> ExactVelocity(double r, double theta, double r1, double r0){
+	std::vector<double> vec(2);
+	vec[0] = r1*r1/(r1*r1 -r0*r0)*(1 - r0*r0/(r*r) * cos(2*theta));
+	vec[1] =  r1*r1/(r1*r1 -r0*r0)*(- r0*r0/(r*r) * sin(2*theta));
+	return vec;
+}
+
+double initialPressure( double x, double y){
+	return 0;
+}
+
+double initialBoundPressure( double x, double y){
+	return 0;
+}
+
+std::vector<double> initialVelocity(double x,double y){
+	std::vector<double> vec(2);
+	vec[0] = 0;
+	vec[1] = 0;
+	return vec;
+}
+std::vector<double> initialBoundVelocity(double x, double y){
+	std::vector<double> vec(2);
+	vec[0] = 1;
+	vec[1] = 0;
+	return vec;
+}
+
+int main(int argc, char** argv)
+{
+	//Preprocessing: mesh and group creation
+	int spaceDim = 2;
+	
+	// Prepare for the mesh
+	cout << "Building mesh" << endl;
+	double r0 = 0.8;
+	double r1 = 6;
+
+	Mesh M;
+	if(argc<2)
+	{
+		    cout << "- DOMAIN : SQUARE" << endl;
+		    cout << "- MESH : CARTESIAN, GENERATED INTERNALLY WITH CDMATH" << endl<< endl;
+		    cout << "Construction of a cartesian mesh" << endl;
+	    double xinf=-0.5;
+	    double xsup= 0.5;
+	    double yinf=-0.5;
+	    double ysup= 0.5;
+	    int nx=50;
+	    int ny=50;
+	    M=Mesh(xinf,xsup,nx,yinf,ysup,ny);
+	    double eps=1e-6;
+	    M.setGroupAtPlan(xsup,0,eps,"RightEdge");
+	    M.setGroupAtPlan(xinf,0,eps,"LeftEdge");
+	    M.setGroupAtPlan(yinf,1,eps,"BottomEdge");
+	    M.setGroupAtPlan(ysup,1,eps,"TopEdge");
+	}
+	else
+	{
+		// ./resources/AnnulusSpiderWeb5x16med
+	    cout << "- MESH:  GENERATED EXTERNALLY WITH SALOME" << endl;
+	    cout << "Loading of a mesh named "<<argv[1] << endl;
+	    string filename = argv[1];
+	    M=Mesh(filename);
+	}
+
+	double kappa = 1;
+	double rho = 1;
+	double c = sqrt(kappa/rho);
+	WaveStaggered myProblem(spaceDim,rho, kappa);
+
+	// Prepare for the initial condition
+	// set the boundary conditions
+	
+	
+	//Initial field creation
+	cout << "Building initial data" << endl;
+	std::map<int ,double> wallPressureMap;
+	std::map<int ,double> wallVelocityMap ;
+	Field Pressure0("pressure", CELLS, M, 1);
+	Field Velocity0("velocity", FACES, M, 1);
+
+	Field ExactVelocityAtCells("ExactVelocityAtCells", CELLS, M, 3); //TODO not used ?
+	Field ExactVelocityAtFaces("ExactVelocityAtFaces", FACES, M, 1);
+	Field ExactVelocityInterpolate("ExactVelocityInterpolate", CELLS, M, 3);
+	
+	for (int j=0; j< M.getNumberOfFaces(); j++ ){
+		Face Fj = M.getFace(j);
+		std::vector<int> idCells = Fj.getCellsId();
+		std::vector<double> vec_normal_sigma(2) ; //TODO = 0!!
+		Cell Ctemp1 = M.getCell(idCells[0]);
+		for(int l=0; l<Ctemp1.getNumberOfFaces(); l++){//we look for l the index of the face Fj for the cell Ctemp1
+			if (j == Ctemp1.getFacesId()[l]){
+				for (int idim = 0; idim < spaceDim; ++idim)
+					vec_normal_sigma[idim] = Ctemp1.getNormalVector(l,idim);
+			}
+		}
+		myProblem.setOrientation(j,vec_normal_sigma);
+		double r =  sqrt(Fj.x()*Fj.x() + Fj.y()*Fj.y());
+		double theta = atan(Fj.y()/Fj.x());
+		std::vector<double > Exact = ExactVelocity(r, theta, r1, r0);
+		double dotprod = 0;
+		for (int k = 0 ; k <Exact.size() ; k++)
+			dotprod += Exact[k] * vec_normal_sigma[k];
+		ExactVelocityAtFaces[j] = dotprod; 
+
+		if(Fj.getNumberOfCells()==2){
+			Cell Ctemp2 = M.getCell(idCells[1]);
+			myProblem.setInteriorIndex(j);
+			Pressure0[idCells[0]] = initialPressure(Ctemp1.x(),Ctemp1.y());
+			Pressure0[idCells[1]] = initialPressure(Ctemp2.x(),Ctemp2.y());
+			std::vector<double > InitialVel = initialVelocity(Fj.x(),Fj.y());
+			double dotprod = 0;
+			for (int k = 0 ; k <InitialVel.size() ; k++)
+					dotprod += InitialVel[k] * vec_normal_sigma[k];
+			Velocity0[j] = dotprod;
+		}
+		else if (Fj.getNumberOfCells()==1){
+			if (( sqrt( Fj.x()*Fj.x()+ Fj.y()*Fj.y() )  ) <= (r0 +r1)/2.0 ){// if face is on interior (wallbound condition) r_int = 1.2 ou 0.8 selon le maillage
+				myProblem.setWallBoundIndex(j);
+				wallVelocityMap[j] =  0;
+			}
+			else {// if face is on exterior (stegger condition) 			
+				myProblem.setSteggerBoundIndex(j);								
+				std::vector<double > BoundaryVel = initialBoundVelocity(Fj.x(),Fj.y());
+				double dotprod = 0;
+				for (int k = 0 ; k <BoundaryVel.size() ; k++)
+					dotprod += BoundaryVel[k] * vec_normal_sigma[k];
+				wallVelocityMap[j] = dotprod;
+				wallPressureMap[j] = initialBoundPressure(Ctemp1.x(),Ctemp1.y());
+			} // building exact solution at faces and its interpolation at cell	
+			ExactVelocityAtFaces[j] = wallVelocityMap[j];
+		}
+	}
+	
+	myProblem.setInitialField(Pressure0);
+	myProblem.setInitialField(Velocity0);
+	myProblem.setboundaryPressure(wallPressureMap);
+	myProblem.setboundaryVelocity(wallVelocityMap);
+
+    // set the numerical method
+	myProblem.setTimeScheme(Explicit);
+    
+    // name of result file
+	string fileName = "WaveStaggered_2DCylinderDeflection";
+
+    // parameters calculation
+	unsigned MaxNbOfTimeStep = 10000000;
+	int freqSave = 500;
+	double cfl = 0.5;
+	double maxTime = 800;
+	double precision = 1e-6;
+
+	myProblem.setCFL(cfl);
+	myProblem.setPrecision(precision);
+	myProblem.setMaxNbOfTimeStep(MaxNbOfTimeStep);
+	myProblem.setTimeMax(maxTime);
+	myProblem.setFreqSave(freqSave);
+	myProblem.setFileName(fileName);
+	myProblem.setSaveFileFormat(VTK);
+	myProblem.saveVelocity(true);
+	myProblem.savePressure(true);
+	myProblem.setVerbose(false);
+	
+	// evolution
+	myProblem.initialize();
+	myProblem.InterpolateFromFacesToCells(ExactVelocityAtFaces, ExactVelocityInterpolate);
+	bool ok = myProblem.run();
+	if (ok)
+		cout << "Simulation "<<fileName<<" is successful !" << endl;
+	else
+		cout << "Simulation "<<fileName<<"  failed ! " << endl;
+
+	cout << "------------ End of calculation !!! -----------" << endl;
+	myProblem.terminate();
+	
+
+	return EXIT_SUCCESS;
+}
---
+++ CoreFlows/examples/C/EulerBarotropicStaggered/WaveStaggered_2DRiemann_StructuredSquares.cxx
@@ -0,0 +1,192 @@
+#include "WaveStaggered.hxx"
+#include "math.h"
+#include <cassert>
+
+using namespace std;
+
+double initialPressure( double z, double discontinuity){
+	if (z < discontinuity)
+		return 2;
+	else
+		return 1;
+}
+
+std::vector<double> initialVelocity(double z, double discontinuity, char Direction){
+	std::vector<double> vec(2);
+	if (z < discontinuity){
+		if (Direction == 'x'){
+			vec[0] = 0;
+			vec[1] = 0;
+		}
+		if (Direction == 'y'){
+			vec[0] = 0;
+			vec[1] = 0;
+		}
+	}
+	else{
+		if (Direction == 'x'){
+			vec[0] = 1;
+			vec[1] = 0;
+		}
+		if (Direction == 'y'){
+			vec[0] = 0;
+			vec[1] = 1;
+		}
+	}
+	return vec;
+}
+
+
+int main(int argc, char** argv)
+{
+	
+	if (argc<2 || (*(argv[1]) != 'x' && *(argv[1]) != 'y') ){
+		cout << "ERROR : you have to give a direction for the pseudo 1d Riemann problem, either 'x' or 'y' ";
+	}
+	else{
+		char Direction = *(argv[1]);
+		//Preprocessing: mesh and group creation
+		int spaceDim = 2;
+		
+		// Prepare for the mesh
+		cout << "Building mesh" << endl;
+		cout << "Construction of a cartesian mesh" << endl;
+		double inf = 0.0;
+		double sup = 1.0;
+		int nx, ny;
+		if (Direction == 'x'){
+			nx=50;
+			if (nx%2 !=0)
+				cout << "ERROR the number of cells should be even" <<endl;
+			ny=3;
+		}
+		else if (Direction == 'y'){
+			nx=3;
+			ny=50;
+			if (ny%2 !=0)
+				cout << "ERROR the number of cells should be even" <<endl;
+		}
+
+		Mesh M=Mesh(inf,sup,nx,inf,sup,ny);
+		double discontinuity = (inf + sup)/2.0;
+		
+		double kappa = 1;
+		double rho = 1;
+		double c = sqrt(kappa/rho);
+		WaveStaggered myProblem(spaceDim,rho, kappa);
+
+		// Prepare for the initial condition
+		// set the boundary conditions
+		
+		//Initial field creation
+		cout << "Building initial data" << endl;
+		std::map<int ,double> wallPressureMap;
+		std::map<int ,double> wallVelocityMap ;
+		Field Pressure0("pressure", CELLS, M, 1);
+		Field Velocity0("velocity", FACES, M, 1);
+		
+		myProblem.setPeriodicFaces(M, Direction);
+		std::map<int,int> FacePeriodicMap = myProblem.getFacePeriodicMap();
+		
+		for (int j=0; j< M.getNumberOfFaces(); j++ ){
+			Face Fj = M.getFace(j);
+			std::vector<int> idCells = Fj.getCellsId();
+			std::vector<double> vec_normal_sigma(2) ; //TODO = 0!!
+			Cell Ctemp1 = M.getCell(idCells[0]);
+			for(int l=0; l<Ctemp1.getNumberOfFaces(); l++){//we look for l the index of the face Fj for the cell Ctemp1
+				if (j == Ctemp1.getFacesId()[l]){
+					for (int idim = 0; idim < spaceDim; ++idim)
+						vec_normal_sigma[idim] = Ctemp1.getNormalVector(l,idim);
+				}
+			}
+			myProblem.setOrientation(j,vec_normal_sigma);
+			double coordLeft, coordRight, coordFace; 
+			if(Fj.getNumberOfCells()==2 ){ // myProblem.IsFaceBoundaryComputedInPeriodic(j)
+				myProblem.setInteriorIndex(j);
+				Cell Ctemp2 = M.getCell(idCells[1]);
+				if (Direction == 'x'){
+					coordLeft = Ctemp1.x();
+					coordRight = Ctemp2.x();
+					coordFace = Fj.x();
+					
+				}
+				else if (Direction == 'y'){
+					coordLeft = Ctemp1.y();
+					coordRight = Ctemp2.y();
+					coordFace = Fj.y() ;
+				}
+				Pressure0[idCells[0]] = initialPressure(coordLeft,discontinuity);
+				Pressure0[idCells[1]] = initialPressure(coordRight,discontinuity);
+				std::vector<double > InitialVel = initialVelocity(coordFace, discontinuity, Direction);
+				double dotprod = 0;
+				for (int k = 0 ; k <InitialVel.size() ; k++)
+						dotprod += InitialVel[k] * vec_normal_sigma[k];
+				Velocity0[j] = dotprod;
+			}
+			else if (Fj.getNumberOfCells()==1  ){ // If boundary face and if periodic check that the boundary face is the computed (avoid passing twice ) 
+				for (int idim = 0; idim <spaceDim; idim ++){
+						if (vec_normal_sigma[idim] < 0)
+							vec_normal_sigma[idim] = -vec_normal_sigma[idim];
+				}
+				myProblem.setOrientation(j,vec_normal_sigma);
+				if  (myProblem.IsFaceBoundaryNotComputedInPeriodic(j) == false && myProblem.IsFaceBoundaryComputedInPeriodic(j) == false)
+					myProblem.setSteggerBoundIndex(j);	
+				if (Direction == 'x'){
+					coordFace = Fj.x();
+					
+				}
+				else if (Direction == 'y'){
+					coordFace = Fj.y() ;
+				}							
+				std::vector<double > BoundaryVel = initialVelocity(coordFace,discontinuity, Direction);
+				double dotprod = 0;
+				for (int k = 0 ; k <BoundaryVel.size() ; k++)
+					dotprod += BoundaryVel[k] * vec_normal_sigma[k];
+				wallVelocityMap[j] = dotprod;
+				wallPressureMap[j] = initialPressure(coordFace,discontinuity);
+			}
+		}
+		
+		myProblem.setInitialField(Pressure0);
+		myProblem.setInitialField(Velocity0);
+		myProblem.setboundaryPressure(wallPressureMap);
+		myProblem.setboundaryVelocity(wallVelocityMap);
+
+		// set the numerical method
+		myProblem.setTimeScheme(Explicit);
+		
+		// name of result file
+		string fileName = "WaveStaggered_2DRiemann_StructuredSquares";
+
+		// parameters calculation
+		unsigned MaxNbOfTimeStep = 1000;
+		int freqSave = 20;
+		double cfl = 0.5;
+		double maxTime = 800;
+		double precision = 1e-6;
+
+		myProblem.setCFL(cfl);
+		myProblem.setPrecision(precision);
+		myProblem.setMaxNbOfTimeStep(MaxNbOfTimeStep);
+		myProblem.setTimeMax(maxTime);
+		myProblem.setFreqSave(freqSave);
+		myProblem.setFileName(fileName);
+		myProblem.setSaveFileFormat(VTK);
+		myProblem.saveVelocity(true);
+		myProblem.savePressure(true);
+		myProblem.setVerbose(false);
+		
+		// evolution
+		myProblem.initialize();
+		bool ok = myProblem.run();
+		if (ok)
+			cout << "Simulation "<<fileName<<" is successful !" << endl;
+		else
+			cout << "Simulation "<<fileName<<"  failed ! " << endl;
+
+		cout << "------------ End of calculation !!! -----------" << endl;
+		myProblem.terminate();
+	}
+
+	return EXIT_SUCCESS;
+}
